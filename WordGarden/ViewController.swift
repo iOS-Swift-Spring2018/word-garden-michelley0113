bb10c812a06ccc530874bb63a523a7a3fad5a320
Michelle Youn <michelleyoun@michelles-air.bc.edu>
2018-02-17 15:37:34 -0500

    Initial Commit

---
+++ WordGarden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by Michelle Youn on 2/17/18.
+//  Copyright © 2018 Michelle Youn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
